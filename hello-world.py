b650628df7a3a4d4de28645bd0f2350ecad1d7f1
22zagiva <49483236+22zagiva@users.noreply.github.com>
2019-04-10 12:45:48 -0400

    Create hello-world.py
    hello world

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Valeriy Zagidulin
+# 04/10/2019
+
+######################
+#  Hello world       #
+#                    #
+#  our first python  #
+#    application     #
+######################
+
+#print "hello, world" to the screen.
+print("hello, world")
